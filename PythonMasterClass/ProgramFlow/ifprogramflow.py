51a95953894d1828a3c3386c6885cf8e8828b07a
JeepGuy <jcbrent@ieee.org>
2019-03-19 19:43:47 -0400

    Add new files

---
+++ PythonMasterClass/ProgramFlow/ifprogramflow.py
@@ -0,0 +1,5 @@
+__author__ = 'dev'
+
+name = input("Pleae enter your name: ")
+age = input("How old are you, {0}?".format(name))
+print(age)
---
+++ PythonMasterClass/Tabs/tabs_and_code_formatting.py
@@ -0,0 +1,7 @@
+__author__='dev'
+
+# Automatic indents 4 spaces to deleanate the code block
+for i in range(1,12):
+    print("No {} squared is {} cubed is {:4}".format(i, i**2, i**4))
+    print('Calculation complete.')
+    print('')
