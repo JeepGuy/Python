acc9557fdea577bfc996b2855034a6d5f35c4f58
JeepGuy <jcbrent@ieee.org>
2019-04-21 15:24:53 -0400

    Add new file

---
+++ Math_Basic_Ops.py
@@ -0,0 +1,46 @@
+
+
+# Round numbers     # to two Decimal places
+print(     round(3.1415926, 2)     )           ;print()
+
+# Absolute Value of a number
+print(     abs(-5)     )                       ; print()
+
+
+# Convert numbers from decimal to different formats
+# -------------------------------------------------
+
+
+# Binary
+print(     bin(20)     )                       ; print()
+
+# Octal Base 8
+print(     oct(20)     )                       ; print()
+
+# Hexidecimal Base 16
+print(     hex(20)     )                       ; print()
+
+# Complex  The first two numbers of the output indicate the new base
+print(     complex(2,3)     )                  ; print()
+# output:  (2+3j)
+
+# Math Library
+# -------------------------------------------------
+
+import math
+
+
+# square root
+print(     math.sqrt(9)     )                       ; print()
+
+# PI
+PI = math.pi
+print(     PI     )                       ; print()
+
+
+# sin
+print(     math.sin(PI/2)     )                       ; print()
+
+
+# Log
+print(     math.log(100, 10)     )                    ; print()
